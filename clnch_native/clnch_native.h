c4b9a466d38a0db24f9fbaa763d33026ba0cd12c
crftwr <craftware@gmail.com>
2017-12-27 16:02:30 -0800

    使ってないNativeクラスを削除

--- clnch_native/clnch_native.h
+++ clnch_native/clnch_native.h
@@ -2,24 +2,4 @@
 #define __CLNCH_NATIVE_H__
 
 
-extern PyTypeObject LockFile_Type;
-#define LockFile_Check(op) PyObject_TypeCheck(op, &LockFile_Type)
-
-struct LockFile_Object
-{
-    PyObject_HEAD
-    HANDLE handle;
-};
-
-
-extern PyTypeObject CheckDir_Type;
-#define CheckDir_Check(op) PyObject_TypeCheck(op, &CheckDir_Type)
-
-struct CheckDir_Object
-{
-    PyObject_HEAD
-    class CheckDir * p;
-};
-
-
 #endif /* __CLNCH_NATIVE_H__ */
